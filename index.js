389b0b35ac71164c9cfb9113cb3dcd5acf92efdf
조혜령 <chohyeryungcho@gmail.com>
2021-09-22 23:20:22 +0900

    mongoose connect!

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express()
 const port = 5000
 
+const config = require("./config/dev");
+
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`starting on port ${port}!`))
