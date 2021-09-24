26a7b3857858dbfd8b7b6d41e2ef22567279cdd3
조혜령 <chohyeryungcho@gmail.com>
2021-09-24 22:00:37 +0900

    feat: server user register

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const { User } = require('./models/User');
+
 const config = require("./config/dev");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
@@ -11,5 +16,17 @@ mongoose.connect(config.mongoURI, {
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+    // 회원가입 시 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다. (status(200) : success)
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    });
+})
 
 app.listen(port, () => console.log(`starting on port ${port}!`))
